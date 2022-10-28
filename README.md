d153bc1beca6a210aabaa3565ed52fd10b01be34
yoonis.ali <yoonismahamoudali@gmail.com>
2022-10-28 09:01:01 -0700

    add js lines

--- README.md
+++
@@ -1,5 +0,0 @@
-Describe mrRogers()
-
-Test: "It should return an array of numbers from 0 to the inputted number."
-Code: mrRogers(3);
-Expected Output: [0, 1, 2, 3]
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-// Business Logic  
+// Business Logic
+
+let roger = function (input) {
+  let numbersArray = [];
+  for (let index = 0; index < input; index++)
+    numbersArray.push(index.toString());
+};
 
 
 
 
 
-
-
-// UI Logic 
+// UI Logic
